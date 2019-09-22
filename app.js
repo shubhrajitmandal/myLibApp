dedcea8033f36a562c9325148944d7627d700add
Shubhrajit Mandal <shubhrajit333@gmail.com>
2019-09-22 09:29:31 +0530

    basic structure

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.DATABASE_URI, {
+  .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log("Connected to myLibrary..."))
+  .then(() => console.log(`Connected to Atlas : myLibrary `))
   .catch(err => console.log(err));
 
 app.engine("handlebars", hbs());
